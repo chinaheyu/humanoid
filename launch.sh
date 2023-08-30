0f4c080a3bf9b123d7ced1405da70a1b1186d7b9
chinaheyu <810130242@qq.com>
2023-08-30 21:12:12 +0800

    update

--- launch.sh
+++ launch.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-SCRIPTPATH=$(readlink -f "$0")
+SCRIPTPATH="$0"
 TOP_DIR=$(dirname "$SCRIPTPATH")
 
 # Sound card volumn
 amixer -c DELI14870 sset PCM 70%
 amixer -c DELI14870 sset Mic 50%
 
-source "${TOP_DIR}/../../../../install/setup.bash"
+source "${TOP_DIR}/../../install/setup.bash"
+echo `pwd`
 ros2 launch humanoid_bringup bringup.py
