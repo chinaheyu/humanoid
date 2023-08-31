9c3626ddffbccefef5be2ec6afe3e893b9ffee4a
chinaheyu <810130242@qq.com>
2023-08-31 09:32:48 +0800

    update

--- launch.sh
+++ launch.sh
@@ -1,14 +1,64 @@
-#/bin/bash
+#/usr/bin/bash
 
 set -e
 
-SCRIPTPATH="$0"
-TOP_DIR=$(dirname "$SCRIPTPATH")
+handle_error() {
+    echo "An error occurred in command: $BASH_COMMAND"
+    exit 1
+}
 
-# Sound card volumn
-amixer -c DELI14870 sset PCM 70%
-amixer -c DELI14870 sset Mic 50%
+find_workspace_directory() {
+    local current_dir=$(readlink -f "$0")
 
-source "${TOP_DIR}/../../install/setup.bash"
-echo `pwd`
+    while [[ "$current_dir" != "/" ]]; do
+        if [[ -d "$current_dir/install" ]]; then
+            echo "$current_dir"
+            return 0
+        fi
+        current_dir="$(dirname "$current_dir")"
+    done
+
+    return -1
+}
+
+wait_for_path() {
+    local path="$1"
+    local interval="$2"
+    
+    while [ ! -e "$path" ]; do
+        echo "Path $path does not exist. Waiting..."
+        sleep "$interval"
+    done
+    
+    echo "Path $path exists!"
+}
+
+wait_all_device_online() {
+    wait_for_path "/dev/input/by-id/usb-DELI_DELI-14870_20080411-event-if03" 1
+    wait_for_path "/dev/snd/by-id/usb-DELI_DELI-14870_20080411-00" 1
+    wait_for_path "/dev/serial/by-id/usb-mjbots_fdcanusb_FFD4048A-if00" 1
+
+    wait_for_path "/dev/serial/by-id/usb-scut_humanoid_205D32834D31-if00" 1
+    wait_for_path "/dev/serial/by-id/usb-scut_humanoid_2064378F5948-if00" 1
+    wait_for_path "/dev/serial/by-id/usb-scut_humanoid_205732834D31-if00" 1
+    wait_for_path "/dev/serial/by-id/usb-scut_humanoid_206F32844D31-if00" 1
+}
+
+# catch error
+trap 'handle_error' ERR
+
+# wait for devices
+wait_all_device_online
+echo "All devices are connected!"
+
+# set sound card volumn
+amixer -q -c DELI14870 sset PCM 70%
+amixer -q -c DELI14870 sset Mic 50%
+echo "Set sound card volumn success!"
+
+# find ros2 workspace
+WORKSPACE_DIR=$(find_workspace_directory)
+
+# launch ros2 packages
+source "${WORKSPACE_DIR}/install/setup.bash"
 ros2 launch humanoid_bringup bringup.py
